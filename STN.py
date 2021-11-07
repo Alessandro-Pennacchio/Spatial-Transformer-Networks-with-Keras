c52ad221725582977dc6c981d0dffa430c8f2fec
AP <71898898+Alessandro-Pennacchio@users.noreply.github.com>
2021-11-07 11:21:07 +0000

    Update STN.py
    Make get_localization_network and get_affine_params a single Sequential Model.
    Allow localization network (with relative phi regression) to be added to the STN Model from outside; retrieve the default model otherwise.
    Verify that the input_shapes of STN and LocalizationNetwork matches.

--- STN.py
+++ STN.py
@@ -2,34 +2,39 @@ import numbers
 
 import tensorflow as tf
 from stn.transformer import affine_grid_generator, bilinear_sampler
-from tensorflow.keras import layers
+from tensorflow.keras import layers, Model
 
-# Spatial transformer localization-network
 def get_localization_network( input_shape ):
 	localization = tf.keras.Sequential( [
+		
+		# Spatial transformer localization-network
 		layers.Conv2D( 8, kernel_size=7, input_shape=input_shape, activation="relu", kernel_initializer="he_normal" ),
 		layers.MaxPool2D( strides=2 ),
 		layers.Conv2D( 10, kernel_size=5, activation="relu", kernel_initializer="he_normal" ),
 		layers.MaxPool2D( strides=2 ),
-	] )
-	return localization
-
-# Regressor for the 3 * 2 affine matrix
-def get_affine_params():
-	fc_loc = tf.keras.Sequential( [
+		
+		# Regressor for the 3 * 2 affine matrix
 		layers.Flatten(),
 		layers.Dense( 32, activation="relu", kernel_initializer="he_normal" ),
 		layers.Dense( 3 * 2, kernel_initializer="zeros", bias_initializer=tf.keras.initializers.Constant( [ 1, 0, 0, 0, 1, 0 ] ) )
 	] )
-	
-	return fc_loc
+	return localization
 
 class STN( layers.Layer ):
 	"""
 	Spatial transformer network forward layer
 	"""
 	
-	def __init__( self, scale=(1, 1), trainable=True, name=None, dynamic=False, **kwargs ):
+	def __init__( self, scale=(1, 1), localizationNetwork=None, trainable=True, name=None, dynamic=False, **kwargs ):
+		
+		self.localizationNetwork_set = False
+		
+		if localizationNetwork is not None:
+			assert isinstance( localizationNetwork, (layers.Layer, Model) )
+			assert tf.reduce_prod( localizationNetwork.output_shape[ 1: ] ) == 6
+			self.localizationNetwork = localizationNetwork
+			self.localizationNetwork_set = True
+		
 		super().__init__( trainable, name, dynamic, **kwargs )
 		
 		if isinstance( scale, numbers.Number ):
@@ -38,18 +43,20 @@ class STN( layers.Layer ):
 			self.scale = scale
 		else:
 			raise Exception
-		
-		self.fc_loc = get_affine_params()
 	
 	def build( self, input_shape ):
 		self.affine_grid_shape = (int( input_shape[ 1 ] * self.scale[ 0 ] ), int( input_shape[ 2 ] * self.scale[ 1 ] ))
-		self.localization = get_localization_network( input_shape[ 1: ] )
+		
+		if not self.localizationNetwork_set:
+			self.localizationNetwork = get_localization_network( input_shape[ 1: ] )
+			self.localizationNetwork_set = True
+		else:
+			assert input_shape == self.localizationNetwork.input_shape
 	
 	def call( self, inputs, *args, **kwargs ):  # Defines the computation from inputs to outputs
 		
-		xs = self.localization( inputs )
+		theta = self.localizationNetwork( inputs )
 		
-		theta = self.fc_loc( xs )
 		theta = tf.reshape( theta, (-1, 2, 3) )
 		
 		grid = affine_grid_generator( self.affine_grid_shape[ 0 ], self.affine_grid_shape[ 1 ], theta )
@@ -60,7 +67,6 @@ class STN( layers.Layer ):
 		return x
 	
 	def get_config( self ):
-		return { "localization": self.localization,
-		         "fc_loc": self.fc_loc,
+		return { "localizationNetwork": self.localizationNetwork,
 		         "affine_grid_shape": self.affine_grid_shape,
 		         }
